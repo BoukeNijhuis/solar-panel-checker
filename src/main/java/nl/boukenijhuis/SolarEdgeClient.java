f7a6d6dca4a15ae6f48be754a40e7ce032de6550
Bouke Nijhuis <bouke.nijhuis@gmail.com>
2026-02-02 19:19:39 +0100

    simplified version

--- src/main/java/nl/boukenijhuis/SolarEdgeClient.java
+++ src/main/java/nl/boukenijhuis/SolarEdgeClient.java
@@ -1,27 +1,27 @@
 package nl.boukenijhuis;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Properties;
 
 public class SolarEdgeClient {
 
-    OkHttpClient client = new OkHttpClient();
+    HttpClient client = HttpClient.newHttpClient();
     private static String URL;
 
     public SolarEdgeClient(Properties properties) {
         URL = "https://monitoringapi.solaredge.com/site/847432/dataPeriod?api_key=" + properties.get("api-key");
     }
 
-    public String getData() throws IOException {
-        Request request = new Request.Builder()
-                .url(URL)
-                .get()
+    public String getData() throws IOException, InterruptedException {
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(URL))
                 .build();
-        Response response = client.newCall(request).execute();
-        return response.body().string();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString()).body();
     }
 }
